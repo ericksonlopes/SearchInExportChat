82d09307764680232b40eb9d32bdf9d16aedde53
Erickson Lopes <ofc.erickson@gmail.com>
2023-03-10 10:31:06 -0300

    add_streamlit

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from src.models.datas import *
-from src.models.dtos import *
-from src.models.filters import *
-from src.models.entities import *
+# from src.models.dtos import *
+# from src.models.filters import *
+# from src.models.entities import *
